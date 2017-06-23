6c353677ece28251f00dc14db3384e2bab0358ed
Local Admin <Local Admin>
2017-06-23 11:34:13 +0100

    another fix

--- tests/consumer_contract.py
+++ tests/consumer_contract.py
@@ -3,7 +3,7 @@ import unittest
 
 from pact import Consumer, Provider
 
-pact = Consumer('UsersConsumer').has_pact_with(Provider('UsersProvider'), port=1234)
+pact = Consumer('UsersConsumer').has_pact_with(Provider('UsersProvider'), port=1235)
 pact.start_service()
 atexit.register(pact.stop_service)
 
