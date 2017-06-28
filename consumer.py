29ec682675c119372ab0b5adc5cd8711585773c1
Local Admin <Local Admin>
2017-06-28 14:36:53 +0100

    Remove user input from consumer.py

--- consumer.py
+++ consumer.py
@@ -7,6 +7,3 @@ def get_user(user_id, api_baseurl):
     """Fetch a user object by user_name from the server."""
     uri = api_baseurl + '/users/' + str(user_id)
     return requests.get(uri, auth=('admin', 'Password123')).json()
-
-user_to_get = input("Enter a user ID to get: ")
-print(get_user(user_to_get, provider_api_path))
